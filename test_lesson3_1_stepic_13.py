8413d7e4976b91b02c56906f8573f00d89532080
A Ogudv <Rune777@yandex.ru>
2020-01-31 16:55:27 +0700

    add test lesson

---
+++ test_lesson3_1_stepic_13.py
@@ -0,0 +1,98 @@
+import unittest
+from selenium import webdriver
+import time
+
+
+class TestAbs(unittest.TestCase):
+    def test_abs1(self):
+        link = "http://suninjuly.github.io/registration1.html"
+
+
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+        input1 = browser.find_element_by_css_selector(
+            "[placeholder='Input your first name']")
+        input1.send_keys("Ivan")
+        input2 = browser.find_element_by_css_selector(
+            "[placeholder='Input your last name']")
+        input2.send_keys("Petrov")
+        input3 = browser.find_element_by_css_selector(
+            "[placeholder='Input your email']")
+        input3.send_keys("Smolensk")
+        input4 = browser.find_element_by_css_selector(
+            "[placeholder='Input your phone:']")
+        input4.send_keys("Smolensk")
+        input5 = browser.find_element_by_css_selector(
+            "[placeholder='Input your address:']")
+        input5.send_keys("Smolensk")
+
+        # Отправляем заполненную форму
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+# находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+# с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        
+
+        time.sleep(10)
+        browser.quit()
+        self.assertEqual("Congratulations! You have successfully registered!",
+                        welcome_text, "Should be absolute value of a number")
+
+    def test_abs2(self):
+        link = "http://suninjuly.github.io/registration2.html"
+
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+        input1 = browser.find_element_by_css_selector(
+            "[placeholder='Input your first name']")
+        input1.send_keys("Ivan")
+        input2 = browser.find_element_by_css_selector(
+            "[placeholder='Input your last name']")
+        input2.send_keys("Petrov")
+        input3 = browser.find_element_by_css_selector(
+            "[placeholder='Input your email']")
+        input3.send_keys("Smolensk")
+        input4 = browser.find_element_by_css_selector(
+            "[placeholder='Input your phone:']")
+        input4.send_keys("Smolensk")
+        input5 = browser.find_element_by_css_selector(
+            "[placeholder='Input your address:']")
+        input5.send_keys("Smolensk")
+
+        # Отправляем заполненную форму
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+# находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+# с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        
+
+        time.sleep(10)
+        browser.quit()
+        self.assertEqual("Congratulations! You have successfully registered!",
+                        welcome_text, "Should be absolute value of a number")
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ test_lesson3_6_stepic_3.py
@@ -0,0 +1,37 @@
+import pytest
+from selenium import webdriver
+import time
+import math
+
+def answer():
+    return math.log(int(time.time()))
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    browser.implicitly_wait(30)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+
+links = ["https://stepik.org/lesson/236895/step/1",
+    "https://stepik.org/lesson/236896/step/1",
+    "https://stepik.org/lesson/236897/step/1",
+    "https://stepik.org/lesson/236898/step/1",
+    "https://stepik.org/lesson/236899/step/1",
+    "https://stepik.org/lesson/236903/step/1",
+    "https://stepik.org/lesson/236904/step/1",
+    "https://stepik.org/lesson/236905/step/1"]
+
+@pytest.mark.parametrize('links', links)
+def test_guest_should_see_login_link(browser, links):
+    link = f"{links}"
+    browser.get(link)
+    browser.find_element_by_css_selector(
+        "[placeholder='Напишите ваш ответ здесь...']").send_keys(answer())
+    browser.find_element_by_class_name("submit-submission").click()
+    assert (browser.find_element_by_class_name("submit-submission").text == "Correct!")
+
